e8a1169f315b584b2a76fea3352cb1146b04e0c1
Michael Raleigh <michael_raleigh@bitbucket.org>
2020-05-04 12:16:56 -0600

    Layout testing.

--- SnookerGame.cpp
+++ SnookerGame.cpp
@@ -110,6 +110,7 @@ void SnookerGame::setup_scoreboard(){
 	draw_number(1, 18, 23, 128, 128, 128);
 	
 	// Initial number of reds on table
+	DrawLine(canvas(), 0, 21, 12, 21, 170, 0, 0);
 	draw_number(1, 2, 23, 128, 128, 128);
 	draw_number(5, 5, 23, 128, 128, 128);
 }
