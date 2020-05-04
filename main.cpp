46f0c00fd89174830e4c788a5c9166fb67b9a38a
Michael Raleigh <michael_raleigh@bitbucket.org>
2020-05-04 11:46:51 -0600

    Fixed bad syntax.

--- main.cpp
+++ main.cpp
@@ -72,9 +72,9 @@ int main(int argc, char *argv[]){
 	delete canvas;
 
 	std::cout << "\n" << std::endl;
-	std::cout << "******************************\n" //<< std::endl;
-	std::cout << "* Exiting LED Matrix Program *\n" //<< std::endl;
-	std::cout << "******************************\n" //<< std::endl;
+	std::cout << "******************************\n"; //<< std::endl;
+	std::cout << "* Exiting LED Matrix Program *\n"; //<< std::endl;
+	std::cout << "******************************\n"; //<< std::endl;
 
 	return 0;
 }
